d0124235dbd1d374dd1479e14d492127b6b63870
Erickhrs <erickhenriquee4444@gmail.com>
2024-10-23 14:24:32 -0300

    fixing bug

--- actions/update_question.php
+++ actions/update_question.php
@@ -77,6 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo "Erro ao atualizar a questão: " . $stmt->error;
         }
     }
+    header('Location: ../system.php');
 } else {
     echo "Método inválido.";
 }
